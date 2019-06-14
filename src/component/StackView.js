8565ee546b784dfb51c180f31a882200d1086b9e
shanthu <shant@sutara.org>
2019-06-14 21:49:42 +0530

    Removing unwanted files

--- src/component/StackView.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Dimensions, Platform } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import BeerDetails from './BeerDetails';
-
-
-
