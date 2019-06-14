37e76f7163463ffa3d6f8566a5cd6bc0ed990ed2
shanthu <shant@sutara.org>
2019-06-14 21:51:14 +0530

    Removing duplicates

--- src/component/Repositories.js
+++ src/component/Repositories.js
@@ -11,6 +11,7 @@ class Repositories extends Component {
         this.state = {
             isLoading: true,
             dataRepository: [],
+            languageInData: [],
             searchText: '',
             text: '',
             value: "All"
@@ -41,7 +42,6 @@ class Repositories extends Component {
     setSearchText(event) {
         let searchText = event.nativeEvent.text;
         this.setState({ searchText });
-
         axios.get('https://api.github.com/users/supreetsingh247/repos')
             .then((responseJson) => {
                 let filteredData = responseJson.data.map((data) => this.filterNotes(searchText, data.language, responseJson));
@@ -59,10 +59,53 @@ class Repositories extends Component {
     onSelect(value, label) {
         this.setState({ value: value });
     }
+    remove_duplicates(arr) {
+        var obj = {};
+        var ret_arr = [];
+        for (var i = 0; i < arr.length; i++) {
+            obj[arr[i]] = true;
+        }
+        for (var key in obj) {
+            ret_arr.push(key);
+        }
+        return ret_arr;
+    }
 
     renderCards = () => {
+        var element = [];
+        var i = 0;
+        this.state.dataRepository.map((data) => {
+            element[i++] = data.language;
+        })
+        console.log("data..." + element)
+        var newElement = [];
+        for (var i = 0; i < element.length; i++) {
+            var count = 0;
+            for (var j = 0; j < element.length; j++) {
+                if (j < i && element[i] == element[j]) {
+                    break;
+                }
+
+
+                if (element[i] == element[j]) {
+                    count = count + 1;
+                }
+                if (j >= element.length - 1) {
+
+                    newElement[i] = element[i]
+
+                }
+            }
+
+        }
+        this.setState({
+            languageInData: newElement
+        })
         return this.state.dataRepository.map((data) => {
-            if ((data.language) == this.state.searchText || (this.state.searchText == '' && (data.language) == this.state.value) || this.state.value == "All" || (data.language) == this.state.value) {
+
+
+
+            if ((data.name.toLowerCase()) == this.state.searchText.toLowerCase() || (this.state.searchText == '' && (data.language) == this.state.value) || this.state.value == "All" || (data.language) == this.state.value) {
                 return <Card key={data.id}>
 
                     <View style={style.headerContainer}>
@@ -79,7 +122,9 @@ class Repositories extends Component {
             }
         }
 
+
         )
+
 
 
     }
@@ -87,9 +132,9 @@ class Repositories extends Component {
         this.setState({ value: value });
     }
     buildMenu = () => {
-        return this.state.dataRepository.map((data) => {
-            console.log("lsangajs" + data.language)
-            return data.language != null ? <Option value={data.language}>{data.language}</Option> : ''
+        return this.state.languageInData.map((data) => {
+
+            return <Option value={data}>{data}</Option>
         }
         )
     }
